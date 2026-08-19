283afbf20431c0895d88c646a3f25fb0ef401b27
Browl3 <gameruphigor@gmail.com>
2026-08-19 16:59:54 -0300

    Update Bancodedados1.sql

--- Bancodedados1.sql
+++ Bancodedados1.sql
@@ -5,8 +5,8 @@ create table cliente (
 
 	id_client int auto_increment primary key,
     nome varchar(100) not null ,
-	cpf varchar(14) not null ,
-
+	cpf varchar(14) not null unique ,
+	email varchar(100) not null unique, 
 
 
 
