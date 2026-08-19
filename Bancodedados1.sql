6d545faf5987dbf4d20f768b9ff6fd887f7e295f
Browl3 <gameruphigor@gmail.com>
2026-08-19 16:58:59 -0300

    Add cpf column to cliente table

--- Bancodedados1.sql
+++ Bancodedados1.sql
@@ -4,8 +4,8 @@ use loja_tech;
 create table cliente (
 
 	id_client int auto_increment primary key,
-    nome varchar(100) not null 
-
+    nome varchar(100) not null ,
+	cpf varchar(14) not null ,
 
 
 
