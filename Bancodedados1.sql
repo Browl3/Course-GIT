cc014cb6ca5f1fce0bb0602cf83bc2ef8750fdd4
Browl3 <gameruphigor@gmail.com>
2026-08-19 17:07:10 -0300

    Update Bancodedados1.sql

--- Bancodedados1.sql
+++ Bancodedados1.sql
@@ -2,20 +2,17 @@ create database loja_tech;
 use loja_tech;
 
 create table cliente (
-
 	id_client int auto_increment primary key,
-    nome varchar(100) not null ,
+	nome varchar(100) not null ,
 	cpf varchar(14) not null unique ,
 	email varchar(100) not null unique, 
 	telefone varchar(16) not null unique,
-    data_nascimento date not null, 
-    cidade varchar(100) not null, 
-    estado char(2) not null ,
-    data_cadastro date not null, 
-    
+	data_nascimento date not null, 
+	cidade varchar(100) not null, 
+	estado char(2) not null ,
+	data_cadastro date not null
 
 );
-
 
 
 
