401d727423b918986909666dfeead88d34b1476d
Browl3 <gameruphigor@gmail.com>
2026-08-20 15:10:16 -0300

    Create 'categoria' table in Bancodedados1.sql
    Added a new table 'categoria' with id, name, and description fields.

--- Bancodedados1.sql
+++ Bancodedados1.sql
@@ -44,6 +44,16 @@ create table fornecedor (
 	cidade varchar(50) not null,
     estado char(2) not null
 );
+
+create table categoria (
+
+ id_categoria int auto_increment primary key,
+ nome varchar(100) not null unique, 
+ descricao varchar(255)
+
+);
+
+
 show tables; 
 
 
